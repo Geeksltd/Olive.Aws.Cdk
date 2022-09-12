9d03fb1a9227844d519c748e726a16d38163b306
m-hoseinzadeh <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2022-09-12 09:35:30 +0100

    Removable s3 buckets

--- 01.Factories/S3Factory.cs
+++ 01.Factories/S3Factory.cs
@@ -10,7 +10,12 @@ namespace Olive.Aws.Cdk
             return new Bucket(scope, name, new BucketProps
             {
                 BucketName = name.ToAwsResourceName(),
-                RemovalPolicy = RemovalPolicy.DESTROY
+                RemovalPolicy = RemovalPolicy.DESTROY,
+
+                // TODO : Check this : https://stackoverflow.com/questions/60199739/find-or-create-s3-bucket-in-cdk
+                AutoDeleteObjects = true
+
+
             });
         }
     }
